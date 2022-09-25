387544d61a9ef4901b9715c2b51f12ba908c20d9
vardhan0604 <harsh062004@gmail.com>
2022-09-25 14:59:05 +0530

    all files added

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("file is connected")
+
+const keyEl=document.getElementById("key");
+
+window.addEventListener("keydown",function(e){
+   //  console.log(e.keyCode)
+    let active =document.querySelector(`div[data-key="${e.keyCode}"]`);
+    active.classList.add("playing");
+    let audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
+   //  console.log(audio)
+     if(!audio){
+        return;
+     }else{
+        audio.currentTime=0
+        audio.play();
+     }
+})
+function removew(f){
+   if(f.propertyName!=="transform"){
+      return;
+   }else{
+      this.classList.remove("playing");\ja
+   }
+}
+
+
+const keys=document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removew));
